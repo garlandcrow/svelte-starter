c52b4e49a28854caae50379a9dde760d73cf5351
garlandcrow <garlandcrow00@gmail.com>
2020-04-04 20:55:58 +0900

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+const production = false; // !process.env.ROLLUP_WATCH
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.html", "./src/**/*.svelte"],
+
+  whitelistPatterns: [/svelte-/, /ripple/],
+
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(production ? [purgecss] : []),
+  ],
+};
