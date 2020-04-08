318b1c4de160f2f7e099731b5acb70246893e06b
garlandcrow <garlandcrow00@gmail.com>
2020-04-08 11:46:40 +0900

    add storybook

--- src/Styles.svelte
+++ src/Styles.svelte
@@ -1,3 +1,7 @@
+<script>
+  import 'smelte/src/tailwind.css'
+</script>
+
 <style global>
   .global-h1 {
     @apply bg-blue-700 text-white;
---
+++ src/components/button.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+
+  export let text = ''
+
+  const dispatch = createEventDispatcher()
+
+  function onClick(event) {
+    dispatch('click', event)
+  }
+</script>
+
+<button class="button bg-blue-700 text-white rounded px-2 py-1" on:click={onClick}>{text}</button>
